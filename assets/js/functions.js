9fe05deee685b926b054bb619c4e36a481435b95
hastere <hastere@crimson.ua.edu>
2016-08-05 15:32:44 -0400

    Evening update!

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
 
   smoothScroll(1000);
+  workBelt();
+  workLoad();
 });
 
 function smoothScroll(duration) {
@@ -14,5 +16,32 @@ function smoothScroll(duration) {
                 scrollTop: target.offset().top
             }, duration);
         }
+    });
+}
+
+function workBelt() {
+    $('.thumb-unit').click(function(){
+        $('.work-belt').css('left', '-100%');
+        $('.work-container').show('800ms');
+    });
+    $('.work-return').click(function(){
+        $('.work-belt').css('left', '0%');
+        $('.work-container').hide('800ms');
+    });
+}
+
+function workLoad() {
+    $.ajaxSetup({cache: true});
+
+    $('.thumb-unit').click(function()   {
+        var $this = $(this),
+            newTitle = $this.find('string').text(),
+            newFolder = $this.data('folder'),
+            spinner = "<div class=\"loader\">Loading....</div>",
+            newHTML = "/work/" + newFolder + ".html" ;
+
+        $('.project-load').html(spinner).load(newHTML);
+        $('.project-title').text(newTitle);
+
     });
 }
